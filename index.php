ccb6317a6b21b3fb1a3ab8afe721356f7af6fec9
elizabeth espeleta <elizabeth.espeleta@gmail.com>
2015-05-08 13:54:27 -0700

    Video 2 done

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+	set_time_limit(0);
+	ini_set('default_socket_timeout', 300);
+	session_start();
+
+
+	define('client_id', 'd99afae27fbc42c8bd2e4516f7b24590');
+	define('client_secret', '94e5978deeb24fa4b6f4b7c2f76e1d77');
+	define('redirectURI', 'http://localhost/Apie/index.php');
+	define('ImageDirectory', 'pics/');
+?>
+
+
+<!-- CLIENT INFO
+CLIENT ID
+d99afae27fbc42c8bd2e4516f7b24590
+CLIENT SECRET
+94e5978deeb24fa4b6f4b7c2f76e1d77
+WEBSITE URL
+http://localhost/Apie/index.php
+REDIRECT URI
+http://localhost/Apie/index.php -->
