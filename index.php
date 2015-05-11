7fbdc8629c9aae611411f3856174d1c229042bc5
elizabeth espeleta <elizabeth.espeleta@gmail.com>
2015-05-11 08:18:58 -0700

    commented on video 3

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
+	//Configuration for our php server
 	set_time_limit(0);
 	ini_set('default_socket_timeout', 300);
 	session_start();
 
-
+	//make constants using define
 	define('client_ID', 'd99afae27fbc42c8bd2e4516f7b24590');
 	define('client_Secret', '94e5978deeb24fa4b6f4b7c2f76e1d77');
 	define('redirectURI', 'http://localhost/Apie/index.php');
@@ -27,6 +28,9 @@ http://localhost/Apie/index.php -->
 	<title></title>
 </head>
 <body>
+		<!--Creating a login for people to go and give approval for our web app to access their Instagram Account
+		After getting approval we are now going to have the information so we can play with it.
+		-->
 		<a href="https:api.instagram/oauth/authorize/?client_id= <?php echo client_ID; ?>^redirect_url=<?php echo redirectURI; ?>response_type=code">LOGIN</a>
 </body>
 </html>
